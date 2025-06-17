e8540bf7341160095ca6b01e10d50d44de282505
jaganyv <jaganyv1@gmail.com>
2025-06-17 11:50:17 +0530

    An Full Stack Web Application For Course Registration System

---
+++ CourseRegistrationFrontend/myscript.js
@@ -0,0 +1,34 @@
+function showCourses(){
+    alert("These Are Your Courses");
+    fetch("http://localhost:8080/courses")
+    .then((response) => response.json())
+    .then((courses) => {
+       const dataTable = document.getElementById("coursetable")
+       courses.forEach(course => {
+        var row = `<tr>
+        <td>${course.courseId}</td>
+        <td>${course.courseName}</td>
+        <td>${course.trainer}</td>
+        <td>${course.durationInWeeks}</td>
+        </tr>`
+        dataTable.innerHTML+=row;
+       });
+    });
+       }
+
+       function showEnrolledStudents(){
+            fetch("http://localhost:8080/courses/enrolled")
+    .then((response) => response.json())
+    .then((students) => {
+       const dataTable = document.getElementById("enrolledtable")
+       students.forEach(student => {
+        var row = `<tr>
+        <td>${student.name}</td>
+        <td>${student.emailId}</td>
+        <td>${student.courseName}</td>
+        </tr>`
+        dataTable.innerHTML+=row;
+       });
+    });
+
+       }
